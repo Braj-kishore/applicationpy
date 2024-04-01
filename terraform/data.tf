33396625b66769fcee860be1522359d3f593b24d
Braj <brajkishore87@gmail.com>
2024-04-01 20:09:43 +0530

    intial commit for app infra

--- terraform/data.tf
+++ terraform/data.tf
@@ -16,7 +16,7 @@ data "azurerm_subnet" "this" {
   for_each             = var.subnets
   name                 = each.value.subnet_name
   virtual_network_name = "${each.value.vnet_name}-${local.resource_name_suffix}"
-  resource_group_name  = data.azurerm_resource_group.this[var.subnets.resource_groups_map_key].name
+  resource_group_name  = data.azurerm_resource_group.this[each.value.resource_groups_map_key].name
 }
 
 data "azurerm_private_dns_zone" "this" {
@@ -27,6 +27,6 @@ data "azurerm_private_dns_zone" "this" {
 
 data "azurerm_key_vault" "this" {
   name                = var.keyvault.name
-  resource_group_name = data.azurerm_resource_group.this[var.app_service_plan.resource_groups_map_key].name
+  resource_group_name = data.azurerm_resource_group.this[var.keyvault.resource_groups_map_key].name
 }
 
